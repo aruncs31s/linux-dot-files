127696a9baff688ee2ae0671dad262a06ac115a7
aruncs31s <aruncs31ss@gmail.com>
2023-07-30 11:32:39 +0530

    added more configs

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ configuration files of linux
 
 [neovim](https://github.com/aruncs31s/linux-dot-files/tree/main/neovim)
 
-![alt-text](https://github.com/aruncs31s/linux-dot-files/blob/main/neovim/2022-04-18-000436_1366x768_scrot.png)
+
+![neovim](./neovim/2022-04-18-000436_1366x768_scrot.png?raw=true)
+
+![neovim.nvchad](./neovim.nvchad/Screenshot_2023-07-30-11-26-38_1366x768.png?raw=true)
---
+++ neovim.nvchad/lua/custom/chadrc.lua
@@ -0,0 +1,27 @@
+---@type ChadrcConfig
+local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "catppuccin",
+  theme_toggle = { "catppuccin", "one_light" },
+  transparency = false,
+
+  statusline = {
+
+    theme = "default", -- default/vscode/vscode_colored/minimal
+
+  },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+}
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
+return M
